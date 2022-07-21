95e8f36b6c05266e5ef9f0f396154a5e5e90a07e
eckig <eckig@users.noreply.github.com>
2022-07-21 09:32:46 +0200

    lazygit

--- .config/nvim/plugin/utils.lua
+++ .config/nvim/plugin/utils.lua
@@ -1,3 +1,5 @@
+local cmd = vim.cmd
+
 -- if deleting a blank line, yank into black-hole register
 local function smart_dd()
   if vim.api.nvim_get_current_line():match("^%s*$") then
@@ -8,3 +10,9 @@ local function smart_dd()
 end
 
 vim.keymap.set( "n", "dd", smart_dd, { noremap = true, expr = true } )
+
+-- Check if we need to reload the file when it changed
+cmd("au FocusGained * :checktime")
+
+-- Highlight on yank
+cmd("au TextYankPost * lua vim.highlight.on_yank {}")
