e1c8909aa2f5698df389605a9a553667bbe35917
eckig <users.noreply.github.com>
2023-10-23 14:12:37 +0200

    .

--- .config/nvim/lua/plugins/disable.lua
+++ .config/nvim/lua/plugins/disable.lua
@@ -1,3 +1,6 @@
 return {
   { "dstein64/vim-startuptime", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "echasnovski/mini.comment", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
 }
