68069db758a9cc372d9723b35951de11479c12c2
eckig <users.noreply.github.com>
2023-11-03 08:47:27 +0100

    .

--- .config/nvim/lua/plugins/disable.lua
+++ .config/nvim/lua/plugins/disable.lua
@@ -4,4 +4,5 @@ return {
   { "echasnovski/mini.comment", enabled = false },
   { "folke/which-key.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
 }
