4d9e1350c1f4ad588d096b327f1a7e54b99a37dc
eckig <users.noreply.github.com>
2024-02-02 09:49:52 +0100

    .

--- .config/nvim/lua/custom/plugins/ui.lua
+++ .config/nvim/lua/custom/plugins/ui.lua
@@ -1,4 +1,20 @@
 return {
 
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    init = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        integrations = {
+          notify = true,
+          treesitter = true,
+          noice = true,
+        },
+      })
+      vim.cmd.colorscheme "catppuccin-latte"
+    end,
+  },
+
   { "nvim-tree/nvim-web-devicons", lazy = true },
 }
