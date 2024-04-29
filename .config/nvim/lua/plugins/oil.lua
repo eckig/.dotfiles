0bd1a84249a9025d5400b4804e9e974210d4cd15
eckig <users.noreply.github.com>
2024-04-29 15:38:50 +0200

    .

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -5,6 +5,8 @@ return {
     event = "VimEnter",
     opts = {
       default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = true,
       view_options = {
         show_hidden = true,
       },
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,22 +1,4 @@
 return {
---   {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000,
---     opts = {
---       transparent_background = vim.fn.has("gui_running") ~= 1,
---       integrations = {
---         treesitter = true,
---         gitsigns = true,
---         notify = true,
---         noice = true,
---       },
---     },
---     config = function()
---       vim.cmd.colorscheme "catppuccin-latte"
---     end
---   },
-
   {
     "EdenEast/nightfox.nvim",
     priority = 1000,
@@ -25,61 +7,6 @@ return {
     end,
   },
 
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      stages = "static",
-      timeout = 3000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-      on_open = function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end,
-    },
-  },
-
-  {
-    "stevearc/dressing.nvim",
-    lazy = true,
-    init = function()
-      vim.ui.select = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.select(...)
-      end
-      vim.ui.input = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.input(...)
-      end
-    end,
-  },
-
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      presets = { bottom_search = true },
-      cmdline = {
-        view = "cmdline",
-        format = {
-          cmdline = { conceal = false },
-          search_down = { conceal = false },
-          search_up = { conceal = false },
-          filter = { conceal = false },
-          lua = { conceal = false },
-          help = { conceal = false },
-          input = { conceal = false },
-        }
-      },
-    },
-  },
-
   -- icons
   { "nvim-tree/nvim-web-devicons", lazy = true },
-
-  -- ui components
-  { "MunifTanjim/nui.nvim", lazy = true },
 }
