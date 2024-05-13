97b1873f7039fbb4d0620b8223638c433ae755b8
eckig <users.noreply.github.com>
2024-05-13 09:44:18 +0200

    .

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,11 +6,53 @@ return {
     config = function()
       require("telescope").setup({
         defaults = {
-          layout_strategy = "vertical",
+          file_ignore_patterns = {
+            "^%.git/",
+            "^%.git$",
+            ".DS_Store",
+          },
+          borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+          preview = { hide_on_startup = true },
+          layout_strategy = 'vertical',
+          layout_config = {
+            vertical = {
+              mirror = true,
+              prompt_position = 'top',
+              width = function(_, cols, _)
+                return math.min( math.floor( 0.85 * cols ), 80 )
+              end,
+              height = function(_, _, rows)
+                return math.floor( rows * 0.85 )
+              end,
+              preview_cutoff = 10,
+              preview_height = 0.4,
+            },
+          },
+
+          results_title = '',
+          sorting_strategy = 'ascending',
+          border = { prompt = { 1, 1, 1, 1 }, results = { 1, 1, 1, 1 }, preview = { 1, 1, 1, 1 }, },
+          path_display = { "filename_first" }, -- trunctate or filename_first
+          mappings = {
+            n = {
+              ['o'] = require('telescope.actions.layout').toggle_preview,
+            },
+            i = {
+              ['<C-o>'] = require('telescope.actions.layout').toggle_preview,
+            },
+          },
           path_display = {
             filename_first = {
               reverse_directories = false
             }
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+          live_grep = {
+            additional_args = { "--hidden" }
           },
         },
       })
