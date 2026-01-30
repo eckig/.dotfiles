8dcd89c71631f9dd53d7f1f31b152562cc995e0d
eckig <users.noreply.github.com>
2026-01-30 12:24:02 +0100

    .

--- .config/nvim/plugin/plug_mini.lua
+++ .config/nvim/plugin/plug_mini.lua
@@ -2,14 +2,7 @@ local now, later = MiniDeps.now, MiniDeps.later
 
 -- Icons
 now(function()
-  -- Set up to not prefer extension-based icon for some extensions
-  local ext3_blocklist = { scm = true, txt = true, yml = true }
-  local ext4_blocklist = { json = true, yaml = true }
-  require('mini.icons').setup({
-    use_file_extension = function(ext, _)
-      return not (ext3_blocklist[ext:sub(-3)] or ext4_blocklist[ext:sub(-4)])
-    end,
-  })
+  require('mini.icons').setup()
   later(MiniIcons.tweak_lsp_kind)
 end)
 
--- .config/nvim/plugin/plug_treesitter.lua
+++ .config/nvim/plugin/plug_treesitter.lua
@@ -1,42 +1,47 @@
-local add, later = MiniDeps.add, MiniDeps.later
+local add, now = MiniDeps.add, MiniDeps.now
 
-later(function()
+now(function()
   add(
   {
     source = 'nvim-treesitter/nvim-treesitter',
     hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
-    checkout = 'master',
   })
-  require("nvim-treesitter.configs").setup(
+
+  local languages =
   {
-    highlight =
-    {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-    ensure_installed =
-    {
-      "bash",
-      "c",
-      "diff",
-      "html",
-      "java",
-      "javascript",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "query",
-      "regex",
-      "toml",
-      "vim",
-      "vimdoc",
-      "xml",
-      "yaml",
-    },
-  })
+    'bash',
+    'c',
+    'diff',
+    'html',
+    'java',
+    'javascript',
+    'json',
+    'lua',
+    'luadoc',
+    'luap',
+    'markdown',
+    'markdown_inline',
+    'query',
+    'regex',
+    'toml',
+    'vim',
+    'vimdoc',
+    'xml',
+    'yaml',
+  }
+  require('nvim-treesitter').install(languages)
+
+  -- Enable tree-sitter after opening a file for a target language
+  local filetypes = {}
+  for _, lang in ipairs(languages) do
+    for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+      table.insert(filetypes, ft)
+    end
+  end
+  local ts_start = function(ev)
+    vim.treesitter.start(ev.buf)
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+  end
+  _Z.new_autocmd('FileType', filetypes, ts_start, 'Start tree-sitter')
 end)
